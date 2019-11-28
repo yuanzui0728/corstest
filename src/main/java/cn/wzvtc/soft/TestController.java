3e8101ed0257d306c255306a3aef0f9ca149bcd8
admin <admin@qq.com>
2019-11-28 09:26:47 +0800

    first commit

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -2,17 +2,41 @@ package cn.wzvtc.soft;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController("/" )
+
+@CrossOrigin(origins = {"http://127.0.0.1:5500"})
+@RestController()
 public class TestController {
 
-    @RequestMapping(value="data.json",method={RequestMethod.GET})
-    public Map<String,String> bookById(){
-        Map<String,String> resultMap=new HashMap<>();
-        resultMap.put("myname","吴港钧");
-        resultMap.put("mynumber","18002040132");
+
+    @RequestMapping(value = "/userinfo")
+    public Map<String, String> userinfo(HttpServletRequest httpServletRequest) {
+
+        Map<String, String> resultMap = new HashMap<>();
+        //判断是否已经登陆,判断SESSION里面是否有我的用户名信息
+        if (httpServletRequest.getSession(true).getAttribute("loginnumber") != null) {
+            resultMap.put("myname", (String) httpServletRequest.getSession().getAttribute("loginnumber"));
+            resultMap.put("mynumber", (String) httpServletRequest.getSession().getAttribute("username"));
+        }
         return resultMap;
     }
+
+    @RequestMapping(value = "/login")
+    public Map<String, String> login(@RequestBody Map<String, String> map, HttpServletRequest httpServletRequest) {
+        String password = map.get("password");
+        String number = map.get("number");
+        Map<String, String> resultMap = new HashMap<>();
+        if ("2012020045".equals(number) && "2012020045".equals(password)) {
+            //将用户名存入到session中
+            httpServletRequest.getSession().setAttribute("loginnumber", number);
+            httpServletRequest.getSession().setAttribute("username", number);
+            resultMap.put("result", "success");
+        }
+        return resultMap;
+    }
+
+
 }
